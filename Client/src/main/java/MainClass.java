7d1e56c9f23e910355e40061feece70695e52c7a
Moldovan Adrian-Alexandru <moldovan.adrian.alexandru03@gmail.com>
2021-06-01 10:27:46 +0300

    Extends MainClass with Application from javafx

--- Client/src/main/java/MainClass.java
+++ Client/src/main/java/MainClass.java
@@ -1,6 +1,14 @@
-public class MainClass {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainClass extends Application {
 
     public static void main(String[] args) {
 
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
     }
 }
