11672bb7ef8901127dc8b80f7bb840121222c29a
Moldovan Adrian-Alexandru <moldovan.adrian.alexandru03@gmail.com>
2021-06-01 10:56:16 +0300

    Create Class chatService who extends ChatlmplVase Base

--- Server/src/main/java/services/ChatService.java
+++ Server/src/main/java/services/ChatService.java
@@ -1,1 +1,35 @@
+package services;
+
+import com.google.protobuf.Timestamp;
+import io.grpc.stub.StreamObserver;
+import protobuf.ChatServiceGrpc;
+import protobuf.ChatServiceOuterClass;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ChatService extends ChatServiceGrpc.ChatServiceImplBase {
+    private static final Set<StreamObserver<ChatServiceOuterClass.ChatMessageFromServer>> observers = ConcurrentHashMap.newKeySet();
+
+    @Override
+    public StreamObserver<ChatServiceOuterClass.ChatMessage> chat(StreamObserver<ChatServiceOuterClass.ChatMessageFromServer> responseObserver) {
+        observers.add(responseObserver);
+
+        return new StreamObserver<ChatServiceOuterClass.ChatMessage>() {
+            @Override
+            public void onNext(ChatServiceOuterClass.ChatMessage value) {
+
+            }
+
+            @Override
+            public void onError(Throwable t) {
+            }
+
+            @Override
+            public void onCompleted() {
+
+            }
+        };
+    }
+}
 
